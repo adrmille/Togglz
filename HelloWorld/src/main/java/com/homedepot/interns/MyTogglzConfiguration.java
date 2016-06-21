2426bbcc710e3e9f59fe5ab09587f5154f4594ac
RupalSanghavi <rsanghavi@smu.edu>
2016-06-21 08:20:45 -0500

    fix attempts, 404 again

--- HelloWorld/src/main/java/com/homedepot/interns/MyTogglzConfiguration.java
+++ HelloWorld/src/main/java/com/homedepot/interns/MyTogglzConfiguration.java
@@ -1,12 +1,15 @@
 package com.homedepot.interns;
 
-import java.io.File;
+import javax.activation.DataSource;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 import org.springframework.stereotype.Component;
 import org.togglz.core.Feature;
 import org.togglz.core.manager.TogglzConfig;
 import org.togglz.core.repository.StateRepository;
-import org.togglz.core.repository.file.FileBasedStateRepository;
+import org.togglz.core.repository.jdbc.JDBCStateRepository;
 import org.togglz.core.user.FeatureUser;
 import org.togglz.core.user.SimpleFeatureUser;
 import org.togglz.core.user.UserProvider;
@@ -18,7 +21,23 @@ public class MyTogglzConfiguration implements TogglzConfig {
     }
 
     public StateRepository getStateRepository() {
-        return new FileBasedStateRepository(new File("/tmp/features.properties"));
+    	 try {
+    		 Context initCtx = new InitialContext();
+    		 Context envCtx = (Context) initCtx.lookup("java:comp/env");
+    		 DataSource ds = (DataSource)
+    		   envCtx.lookup("jdbc/switches");
+ 
+    		//DataSource dataSource = (DataSource) new InitialContext().lookup("jdbc/switches");
+    		
+             //InitialContext context = new InitialContext();
+             //DataSource dataSource = (DataSource) context.lookup("jboss/datasources/ExampleDS");
+             return new JDBCStateRepository((javax.sql.DataSource) ds, "MYTABLE");
+
+         } catch (NamingException e) {
+             throw new IllegalArgumentException("Could not find datasource");
+         }
+
+    	//return new FileBasedStateRepository(new File("/tmp/features.properties"));
     }
 
     public UserProvider getUserProvider() {
