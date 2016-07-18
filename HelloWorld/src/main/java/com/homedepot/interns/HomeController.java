6c6005fdac5163cef1216428063dfc6195f8b11b
RupalSanghavi <rsanghavi@smu.edu>
2016-07-18 14:51:57 -0500

    Parsing XML into Map accurately

--- HelloWorld/src/main/java/com/homedepot/interns/HomeController.java
+++ HelloWorld/src/main/java/com/homedepot/interns/HomeController.java
@@ -57,8 +57,8 @@ public class HomeController {
 	@RequestMapping(value = "/time", method = RequestMethod.GET)
 	public ModelAndView timing() throws Exception{
 		logger.info("User has entered timing page");
-		//MyOptionsRegistry me = MyOptionsRegistry.getInstance();
-		//me.populate();
+		MyOptionsRegistry me = MyOptionsRegistry.getInstance();
+		me.populate();
 		return new ModelAndView("time", "command", new User());
 	}
 	
@@ -149,20 +149,7 @@ public class HomeController {
 		op.listFeatures();
 		
 		MyCachingStateRepo cache = MyCachingStateRepo.getInstance();
-		try{
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		String json = "{\"FEATURE_THREE\":{\"state\":null,\"timestamp\":1468854064451,\"last_UPD_SYSUSR_ID\":null,\"last_UPD_TS\":null,\"app_ENV\":null,\"strategy_ID\":null,\"strategy_PARAMS\":null,\"feature_ID\":null},\"HFMobileTypeAhead\":{\"state\":{\"feature\":\"HFMobileTypeAhead\",\"enabled\":false,\"strategyId\":null,\"users\":[],\"parameterMap\":{},\"parameterNames\":[]},\"timestamp\":1468854064592,\"last_UPD_SYSUSR_ID\":\"TAUSER02\",\"last_UPD_TS\":\"2016-06-27 13:03:14.0\",\"app_ENV\":\"live\",\"strategy_ID\":null,\"strategy_PARAMS\":null,\"feature_ID\":\"5522016d-483a-11e6-9da2-08002727ab87\"},\"MyAccountSOCCFeature\":{\"state\":{\"feature\":\"MyAccountSOCCFeature\",\"enabled\":true,\"strategyId\":null,\"users\":[],\"parameterMap\":{},\"parameterNames\":[]},\"timestamp\":1468854064554,\"last_UPD_SYSUSR_ID\":\"TAUSER02\",\"last_UPD_TS\":\"2016-06-29 16:19:01.0\",\"app_ENV\":\"live\",\"strategy_ID\":null,\"strategy_PARAMS\":null,\"feature_ID\":\"5521fffa-483a-11e6-9da2-08002727ab87\"},\"FEATURE_ONE\":{\"state\":null,\"timestamp\":1468854064374,\"last_UPD_SYSUSR_ID\":null,\"last_UPD_TS\":null,\"app_ENV\":null,\"strategy_ID\":null,\"strategy_PARAMS\":null,\"feature_ID\":null},\"PipSearchNav2BannerFeatureSwitch\":{\"state\":{\"feature\":\"PipSearchNav2BannerFeatureSwitch\",\"enabled\":true,\"strategyId\":null,\"users\":[],\"parameterMap\":{},\"parameterNames\":[]},\"timestamp\":1468854064509,\"last_UPD_SYSUSR_ID\":\"TAUSER02\",\"last_UPD_TS\":\"2016-05-25 13:43:30.0\",\"app_ENV\":\"live\",\"strategy_ID\":null,\"strategy_PARAMS\":null,\"feature_ID\":\"5521fafe-483a-11e6-9da2-08002727ab87\"},\"FEATURE_TWO\":{\"state\":null,\"timestamp\":1468854064420,\"last_UPD_SYSUSR_ID\":null,\"last_UPD_TS\":null,\"app_ENV\":null,\"strategy_ID\":null,\"strategy_PARAMS\":null,\"feature_ID\":null}}";
-		Map<String, MyCacheEntry> temp = null;  //cache.getMap();
-		temp = mapper.readValue(json, new TypeReference<Map<String, MyCacheEntry>>(){});
-		System.out.println(temp);
-		} catch (JsonGenerationException e){
-			e.printStackTrace();
-		} catch (JsonMappingException e){
-			e.printStackTrace();
-		} catch (IOException e){
-			e.printStackTrace();
-		}
+		
 		/*String json = "{\"FEATURE_THREE\":{\"state\":null,\"timestamp\":1468854064451,\"last_UPD_SYSUSR_ID\":null,\"last_UPD_TS\":null,\"app_ENV\":null,\"strategy_ID\":null,\"strategy_PARAMS\":null,\"feature_ID\":null},\"HFMobileTypeAhead\":{\"state\":{\"feature\":\"HFMobileTypeAhead\",\"enabled\":false,\"strategyId\":null,\"users\":[],\"parameterMap\":{},\"parameterNames\":[]},\"timestamp\":1468854064592,\"last_UPD_SYSUSR_ID\":\"TAUSER02\",\"last_UPD_TS\":\"2016-06-27 13:03:14.0\",\"app_ENV\":\"live\",\"strategy_ID\":null,\"strategy_PARAMS\":null,\"feature_ID\":\"5522016d-483a-11e6-9da2-08002727ab87\"},\"MyAccountSOCCFeature\":{\"state\":{\"feature\":\"MyAccountSOCCFeature\",\"enabled\":true,\"strategyId\":null,\"users\":[],\"parameterMap\":{},\"parameterNames\":[]},\"timestamp\":1468854064554,\"last_UPD_SYSUSR_ID\":\"TAUSER02\",\"last_UPD_TS\":\"2016-06-29 16:19:01.0\",\"app_ENV\":\"live\",\"strategy_ID\":null,\"strategy_PARAMS\":null,\"feature_ID\":\"5521fffa-483a-11e6-9da2-08002727ab87\"},\"FEATURE_ONE\":{\"state\":null,\"timestamp\":1468854064374,\"last_UPD_SYSUSR_ID\":null,\"last_UPD_TS\":null,\"app_ENV\":null,\"strategy_ID\":null,\"strategy_PARAMS\":null,\"feature_ID\":null},\"PipSearchNav2BannerFeatureSwitch\":{\"state\":{\"feature\":\"PipSearchNav2BannerFeatureSwitch\",\"enabled\":true,\"strategyId\":null,\"users\":[],\"parameterMap\":{},\"parameterNames\":[]},\"timestamp\":1468854064509,\"last_UPD_SYSUSR_ID\":\"TAUSER02\",\"last_UPD_TS\":\"2016-05-25 13:43:30.0\",\"app_ENV\":\"live\",\"strategy_ID\":null,\"strategy_PARAMS\":null,\"feature_ID\":\"5521fafe-483a-11e6-9da2-08002727ab87\"},\"FEATURE_TWO\":{\"state\":null,\"timestamp\":1468854064420,\"last_UPD_SYSUSR_ID\":null,\"last_UPD_TS\":null,\"app_ENV\":null,\"strategy_ID\":null,\"strategy_PARAMS\":null,\"feature_ID\":null}}";
 		HashMap<String,MyCacheEntry> map = new Gson().fromJson(json, new TypeToken<HashMap<String, MyCacheEntry>>(){}.getType());
 		System.out.println(map);*/
